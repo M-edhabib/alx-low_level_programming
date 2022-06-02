286011446900b836202b98b91f923d85b17d7b25
M-edhabib <medhabib1597@gmail.com>
2022-06-02 04:34:10 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main print message
+ * return 0 if succesful
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+retunr (0);
+}
